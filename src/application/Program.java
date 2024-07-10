bf969ffd3b32b42a3edb492377b9be0c187ff94d
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-07-09 21:24:58 -0300

    Seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         Department dp = new Department(2, "Eletronics");
-
-        System.out.println(dp);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 1750.0, dp);
+        System.out.println(seller);
     }
 }
