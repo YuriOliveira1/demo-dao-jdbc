f86b3a6dc00f698231cf1e3cc5e924a669427c63
Yuri de Oliveira <161542848+YuriOliveira1@users.noreply.github.com>
2024-07-09 23:07:40 -0300

    Add Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,5 +31,11 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TESTE 4: seller insert ===");
+        Seller newSeller = new Seller(null, "John", "John@gamil.com", new Date(), 1750.50, dp);
+
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
